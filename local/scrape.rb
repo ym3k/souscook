e43daa2971638cf0168ee8711f612084bdf0d3f5
ym3k <ym3k@icloud.com>
2018-02-12 19:32:07 +0900

    スクレイピングテストコード作成中

--- local/scrape.rb
+++ local/scrape.rb
@@ -1,8 +1,12 @@
 require 'nokogiri'
 require 'open-uri'
+require 'uri'
 
-url = 'https://cookpad.com/search/%E8%B1%9A%E8%82%89%20%E3%83%94%E3%83%BC%E3%83%9E%E3%83%B3%20%E7%8E%89%E3%81%AD%E3%81%8E%E3%80%80%E4%BA%BA%E5%8F%82'
-#url = 'http://qiita.com/search?q=ruby'
+cpsite = 'https://cookpad.com'
+items = ARGV.join(' ')
+url = URI.escape(cpsite + '/search/' + items)
+
+#url = URI.escape('https://cookpad.com/search/豚肉 じゃがいも')
 
 charset = nil
 
@@ -13,6 +17,7 @@ end
 
 doc = Nokogiri::HTML.parse(html, nil, charset)
 doc.xpath('//div[@class="recipe-preview"]').each do |node|
-  p node.css('a')[0].attributes['href'].value,
+  p cpsite + node.css('a')[0].attributes['href'].value,
+    node.css('img')[0].attributes['src'].value,
     node.css('a')[1].inner_text
 end
