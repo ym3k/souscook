e03ab91807b6d281ada1cca95673c25481f4bbd7
ym3k <ym3k@icloud.com>
2018-02-20 22:49:56 +0900

    セッション管理機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update]
-  #before_action :set_login, only: [:edit, :update]
+  before_action :set_login, only: [:edit, :update]
 
   # GET /users
   # GET /users.json
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root 'foodstoks#index'
+
   resources :users, only: [:new, :create, :show, :update, :edit]
+  resources :sessions, only: [:new, :create, :destroy]
+
   resources :recipes
   resources :foodstocks
 
